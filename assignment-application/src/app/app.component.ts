219919ca2966f0867937f2695bb1f0bf7f0adad8
Vishwateja Kandukuri <bunny990@live.in>
2021-10-22 11:05:04 +0530

    auto complete commit

--- assignment-application/src/app/app.component.ts
+++ assignment-application/src/app/app.component.ts
@@ -1,8 +1,11 @@
 import { Component, ViewChild } from '@angular/core';
+import { FormControl } from '@angular/forms';
 import { MatMenuTrigger } from '@angular/material/menu';
 import { GooglePlaceDirective } from 'ngx-google-places-autocomplete';
 import { Address } from 'ngx-google-places-autocomplete/objects/address';
 import { Options } from 'ngx-google-places-autocomplete/objects/options/options';
+import { Observable } from 'rxjs/internal/Observable';
+import { map, catchError, startWith } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -10,28 +13,45 @@ import { Options } from 'ngx-google-places-autocomplete/objects/options/options'
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  @ViewChild(MatMenuTrigger) trigger!: MatMenuTrigger;
-  openlist() {
-    this.trigger.openMenu();
+  simpleOptions = ["One", "Two", "Three"];
+  listOfValues : string[] = [];
+  previousLocations: string[] = ["warangal", "hyderabad"];
+
+  //
+  myControl: FormControl = new FormControl();
+  filteredOptions!: Observable<string[]>;
+
+  ngOnInit() {
+    this.filteredOptions = this.myControl.valueChanges
+      .pipe(
+        startWith(''),
+        map((val: string) => this.filter(val))
+      );
   }
-  @ViewChild("placesRef") placesRef! : GooglePlaceDirective;
-  fieldValue: any;
-  listOfValues : string[] = []
-  ngAfterViewInit() {
-    this.placesRef.options.componentRestrictions = { country: 'SG' }
-    this.placesRef.options.fields = ["formatted_address", "geometry", "place_id"]
+
+  filter(val: string): string[] {
+    return this.previousLocations.filter(option =>
+      option.toLowerCase().indexOf(val.toLowerCase()) === 0);
   }
-  public handleAddressChange(address: Address) {
-    // Do some stuff
-  }
+  //
+  
   public addData() {
-    let tempList = this.fieldValue.split(',');
+    let val = this.myControl.value;
+    let tempList = val.split(',');
     tempList.forEach((element: string) => {
-      if(!this.listOfValues.includes(element)){
+      if (!this.listOfValues.includes(element)) {
         this.listOfValues.push(element);
       }
     });
-    this.fieldValue='';
+    tempList.forEach((element: string) => {
+      if(!this.previousLocations.includes(element)) {
+        this.previousLocations.push(element);
+      }
+    });
+    this.resetForm();
+  }
+  public resetForm() {
+    this.myControl.setValue('');
   }
   public removeData(item: string){
     this.listOfValues.forEach((element, index) => {
--- assignment-application/src/app/app.module.ts
+++ assignment-application/src/app/app.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { GooglePlaceModule } from 'ngx-google-places-autocomplete';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { MatMenuModule} from '@angular/material/menu';
-
+import {MatSelectModule} from '@angular/material/select';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatMenuModule} from '@angular/material/menu';
+import {MatAutocompleteModule} from '@angular/material/autocomplete';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 @NgModule({
   declarations: [
     AppComponent
@@ -16,7 +20,13 @@ import { MatMenuModule} from '@angular/material/menu';
     AppRoutingModule,
     GooglePlaceModule,
     FormsModule,
-    MatMenuModule
+    MatSelectModule,
+    BrowserAnimationsModule,
+    MatMenuModule,
+    MatAutocompleteModule,
+    ReactiveFormsModule,
+    MatInputModule,
+    MatFormFieldModule
   ],
   providers: [],
   bootstrap: [AppComponent]
